544e0a6ab6d8910494a864dc665331fa171465c7
Suhanimathur1208 <81302633+Suhanimathur1208@users.noreply.github.com>
2021-04-02 18:03:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var movingRect,fixedRect;
+
+function setup() {
+  createCanvas(1200,800);
+
+  movingRect=createSprite(200,300,100,40);
+  movingRect.shapeColor="green";
+  movingRect.debug=true;
+
+  fixedRect=createSprite(300,300,40,100);
+  fixedRect.shapeColor="green";
+  fixedRect.debug=true;
+
+}
+
+function draw() {
+  background(100,30,50); 
+
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+
+  //right - MovingRect HX   
+if(movingRect.x-fixedRect.x <=  fixedRect.width/2 +  movingRect.width/2 &&
+  
+  fixedRect.y-movingRect.y<= fixedRect.height/2+movingRect.height/2 && 
+  
+  fixedRect.x-movingRect.x<=fixedRect.width/2+movingRect.width/2 &&
+
+  movingRect.y-fixedRect.y<=fixedRect.height/2+movingRect.height/2 ){
+
+  movingRect.shapeColor="red";
+  fixedRect.shapeColor="red";
+
+
+}
+
+else{
+
+  fixedRect.shapeColor="green";
+  movingRect.shapeColor="green";
+
+}
+
+ 
+  drawSprites();
+} 
+
+
+
+
